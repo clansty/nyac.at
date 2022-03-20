be93f1f003d60870a84183c4071ba12784549eac
Clansty <i@gao4.pw>
2022-03-20 19:46:14 +0800

    perf: favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterView } from 'vue-router';
 import styles from './App.module.sass';
 import random from './utils/random';
+import favicon from './assets/favicon.webp';
 
 const BACKGROUNDS = [styles.background1, styles.background2, styles.background3, styles.background4, styles.background5];
 
@@ -8,6 +9,9 @@ export default defineComponent({
   setup() {
     useHead({
       title: '凌莞喵～',
+      link: [
+        { rel: 'icon', href: favicon, type: 'image/webp' },
+      ],
     });
     const randomBackground = ref('');
     if (!import.meta.env.SSR) {
