c9baec355d46c6284c93f089789bee06c6232163
Clansty <i@gao4.pw>
2022-04-02 22:08:44 +0800

    perf: 加入文章页面的 headers

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -18,6 +18,29 @@ const Component = defineComponent({
     const { default: meta } = await postData[postMetaPath(slug)]() as { default: PostInfo };
     const { default: Content } = await postData[postContentPath(slug)]();
 
+    useHead({
+      title: `${meta.title} — 凌莞咕噜咕噜～`,
+      link:[
+        { rel: 'canonical', href: `https://nyac.at/posts/${slug}` },
+      ],
+      meta:[
+        { name: 'description', content: meta.desc },
+        { name: 'author', content: 'Clansty' },
+        { property: 'og:title', content: meta.title },
+        { property: 'og:type', content: 'article' },
+        { property: 'og:description', content: meta.desc },
+        { property: 'og:image', content: meta.banner },
+        { property: 'og:url', content: `https://nyac.at/posts/${slug}` },
+        { property: 'article:published_time', content: new Date(meta.date).toISOString() },
+        { property: 'article:modified_time', content: new Date(meta.date).toISOString() },
+        { property: 'twitter:card', content: 'summary_large_image' },
+        { property: 'twitter:title', content: meta.title },
+        { property: 'twitter:description', content: meta.desc },
+        { property: 'twitter:image', content: meta.banner },
+        { property: 'twitter:url', content: `https://nyac.at/posts/${slug}` },
+      ]
+    })
+
     return () => (
       <BlogLayout postInfo={meta}>
         <div class="postContent">
