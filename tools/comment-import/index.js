fae6bb81a9def125bed7a2d27b4adb774940f4ce
Clansty <i@gao4.pw>
2022-04-02 22:53:33 +0800

    feat: import comments

---
+++ tools/comment-import/index.js
@@ -0,0 +1,25 @@
+const oldData = require('./export.json');
+const fs = require('fs');
+const path = require('path');
+
+const out = {};
+
+for (const oldComment of oldData) {
+  if (!out[oldComment.postId]) {
+    out[oldComment.postId] = [];
+  }
+  out[oldComment.postId].push({
+    avatar: oldComment.avatar,
+    content: oldComment.content,
+    date: new Date(oldComment.date.$date).getTime(),
+    email: oldComment.email,
+    url: oldComment.url,
+    username: oldComment.username,
+  });
+}
+
+fs.mkdirSync(path.join('dist', 'comments'), { recursive: true });
+for (const key of Object.keys(out)) {
+  fs.writeFile(path.join('dist', 'comments', `${key}.json`), JSON.stringify(out[key], undefined, 0), () => {
+  });
+}
