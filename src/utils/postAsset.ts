1f1ca2b2ef96320d9a3761eed83bf5c063a5b989
Clansty <i@gao4.pw>
2022-04-04 19:46:04 +0800

    fix: 文章里面的一些图片裂开了

--- src/utils/postAsset.ts
+++ src/utils/postAsset.ts
@@ -1,7 +1,7 @@
 const assets = import.meta.globEager('../../data/posts/*/*.{webp,png,jpg,jpeg,gif}');
 
 export default (slug: string, name: string) => {
-  const asset = assets[`../../data/posts/${slug}/${name}`];
+  const asset = assets[`../../data/posts/${slug}/${decodeURIComponent(name)}`];
   if (!asset) return null;
   return asset.default as string;
 }
