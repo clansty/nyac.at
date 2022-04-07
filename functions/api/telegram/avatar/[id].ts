f5b1992d426cfd353730af9e952b4bb659c2bd94
Clansty <i@gao4.pw>
2022-04-07 21:48:48 +0800

    fix: any script 坏！

--- functions/api/telegram/avatar/[id].ts
+++ functions/api/telegram/avatar/[id].ts
@@ -6,17 +6,15 @@ export const onRequestGet: PagesFunction<{
 }, 'id'> = async ({ env, params }) => {
   let cache = await env.LINK_STORE.get(`avatar:${params.id}`);
   if (!cache) {
-    const avatars = await callTgFunc('getUserProfilePhotos', {
+    const avatars = (await callTgFunc('getUserProfilePhotos', {
       user_id: Number(params.id),
       limit: 1,
-    }, env.BOT_TOKEN);
-    await env.LINK_STORE.put('test1', JSON.stringify(avatars));
+    }, env.BOT_TOKEN) as any).result.photos;
     const avatar = avatars[0][avatars[0].length - 1];
     const download: any = await callTgFunc('getFile', {
       file_id: avatar.file_id,
     }, env.BOT_TOKEN);
-    await env.LINK_STORE.put('test2', JSON.stringify(avatars));
-    cache = `https://api.telegram.org/file/bot${env.BOT_TOKEN}/${download.file_path}`;
+    cache = `https://api.telegram.org/file/bot${env.BOT_TOKEN}/${download.result.file_path}`;
     await env.LINK_STORE.put(`avatar:${params.id}`, cache, {
       expirationTtl: 60 * 60,
     });
