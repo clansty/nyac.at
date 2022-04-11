aeb2c600a29241b221173ec599ff293f31ed50c8
Clansty <i@gao4.pw>
2022-04-11 16:39:01 +0800

    perf: 增加果味

--- src/components/FileView/FileEntry.tsx
+++ src/components/FileView/FileEntry.tsx
@@ -4,6 +4,8 @@ import { CustomFileInfo } from '~/types/FolderInfo';
 import styles from './FileEntry.module.sass';
 import { RouterLink } from 'vue-router';
 import FileIcon from '~/components/FileView/FileIcon';
+import chevronRight from '@iconify-icons/akar-icons/chevron-right';
+import Icon from '~/components/Icon';
 
 export default defineComponent({
   props: {
@@ -12,13 +14,20 @@ export default defineComponent({
     extra: Object as PropType<CustomFileInfo>,
   },
   setup({ file, extra, path }) {
+    const route = useRoute();
+    const isFolder = file?.type === 1 || extra?.isFolder;
     const inner = <>
       <div class={styles.icon}>
-        <FileIcon path={path} isFolder={file?.type === 1 || extra?.isFolder}/>
+        <FileIcon path={path} isFolder={isFolder}/>
       </div>
       <div class={styles.title}>
         {extra?.title || extra?.name || file?.name}
       </div>
+      {isFolder && (
+        <div class={styles.folderArrow}>
+          <Icon icon={chevronRight}/>
+        </div>
+      )}
     </>;
     if (extra?.url) {
       return () => (
@@ -28,7 +37,8 @@ export default defineComponent({
       );
     }
     return () => (
-      <RouterLink to={`/files${path.replace(/#/g, '%23')}`} class={styles.fileEntry}>
+      <RouterLink to={`/files${path.replace(/#/g, '%23')}`}
+                  class={`${styles.fileEntry} ${`/${route.params.path}`.startsWith(path) && styles.selected}`}>
         {inner}
       </RouterLink>
     );
