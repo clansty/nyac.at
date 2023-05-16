1a0c20b2be99f8bb31eba5d7a270dcb0b8e2c6d2
Clansty <i@gao4.pw>
2023-05-17 02:52:25 +0800

    Revert "remove shares"
    This reverts commit abb1d36e5e305bf2987f0bf2fc1f7e1a29832cbf.

---
+++ src/pages/shares.tsx
@@ -0,0 +1,33 @@
+import styles from './shares.module.sass';
+import BackButton from '~/components/BackButton';
+import Loading from '~/components/Loading';
+
+export default defineComponent({
+  setup() {
+    useHead({
+      title: '凌莞的喵喵喵碎碎念',
+      link: [
+        { rel: 'canonical', href: 'https://clansty.com/shares' },
+      ],
+      meta: [
+        { property: 'og:url', content: 'https://clansty.com/shares' },
+        { name: 'description', content: '凌莞的喵喵喵碎碎念和奇奇怪怪的分享' },
+        { property: 'og:title', content: '凌莞的喵喵喵碎碎念' },
+        { property: 'og:description', content: '凌莞的喵喵喵碎碎念和奇奇怪怪的分享' },
+        { property: 'twitter:title', content: '凌莞的喵喵喵碎碎念' },
+        { property: 'twitter:description', content: '凌莞的喵喵喵碎碎念和奇奇怪怪的分享' },
+        { name: 'robots', content: 'noindex' },
+      ],
+    });
+
+    return () => (
+      <div class={styles.sharesContainer}>
+        <BackButton to="/" class={styles.back}/>
+        <iframe src="https://tg.0w0.wiki?emb" frameborder="0" allowtransparency/>
+        <div class={styles.loading}>
+          <Loading color="#60A5FA"/>
+        </div>
+      </div>
+    );
+  },
+});
