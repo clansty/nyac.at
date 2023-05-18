93ff293a19863e97c494dc81b846633d08466bfe
Clansty <i@gao4.pw>
2023-05-18 22:02:31 +0800

    log

--- functions/posts/_middleware.ts
+++ functions/posts/_middleware.ts
@@ -1,4 +1,5 @@
 export const onRequest: PagesFunction = async (context) => {
+  console.log(context.request.headers.get('accept'));
   if (!context.request.url.endsWith('.json') && context.request.headers.get('accept') === 'application/activity+json') {
     return await context.env.ASSETS.fetch(context.request.url + '.json', context.request);
   }
