f6bc27d882d22e73bcda8bb5a69c8ea117a3b955
Clansty <i@gao4.pw>
2023-11-26 22:12:32 +0800

    修复 mobile safari 滚动问题

--- src/components/ScrollContainer.tsx
+++ src/components/ScrollContainer.tsx
@@ -7,24 +7,33 @@ export default defineComponent({
     const scroll = ref<HTMLDivElement>();
     const content = ref<HTMLDivElement>();
 
-    useIntervalFn(() => {
-      if (!dummy.value) return;
-      dummy.value.style.height = content.value?.scrollHeight + 'px';
-    }, 200, { immediate: true, immediateCallback: true });
-
     const inScrollTop = ref(false);
+    const isMobileSafari = ref(false);
 
     expose({
       scrollUp() {
-        console.log('scrollUp');
         if (!content.value) return;
         inScrollTop.value = true;
         content.value.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-        scroll.value.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+        isMobileSafari.value || scroll.value.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
 
         setTimeout(() => inScrollTop.value = false, 1000);
       },
     });
+
+    if (navigator.userAgent.toLowerCase().includes('mobile') && navigator.userAgent.toLowerCase().includes('safari')) {
+      isMobileSafari.value = true;
+      return () => <>
+        {h(slots.default()[0], {
+          ref: content,
+        })}
+      </>
+    }
+
+    useIntervalFn(() => {
+      if (!dummy.value) return;
+      dummy.value.style.height = content.value?.scrollHeight + 'px';
+    }, 200, { immediate: true, immediateCallback: true });
 
     return () => <>
       <Teleport to="body">
