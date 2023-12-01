a587e8b6b3a2543cb7ceb121ed547baf40969b43
Clansty <i@gao4.pw>
2023-12-01 11:39:11 +0800

    修复 service worker globPatterns

--- vite.config.ts
+++ vite.config.ts
@@ -97,7 +97,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: [
-          'assets/*.*.*',
+          'x_assets_x/*.*.*',
         ],
         globIgnores: [
           '*.html',
