da6fabb75b15485b275324b2830ab0be8aad3a5e
Clansty <i@gao4.pw>
2024-06-01 12:31:42 +0800

    fix

--- src/modules/posthog.ts
+++ src/modules/posthog.ts
@@ -2,6 +2,7 @@ import posthog from "posthog-js";
 import type { UserModule } from '~/types'
 
 export const install: UserModule = ({ app }) => {
+  if (import.meta.env.SSR) return;
   app.config.globalProperties.$posthog = posthog.init(
     'phc_4BFetd09h1ClEounK6MrswNPNxvCTyjbFFFyIH8BkY6',
     {
