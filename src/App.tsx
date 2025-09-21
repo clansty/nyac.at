1b05f2e5e4b41d8617ae83079ff0098103e74fb5
Clansty <i@gao4.pw>
2025-09-21 18:12:47 +0800

    remove some background

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { RouterView } from 'vue-router';
 import styles from './App.module.sass';
 import random from './utils/random';
-import favicon from './assets/clansty_profile_picture_clean_fix_fair.webp';
+import favicon from './assets/bg3.webp';
 import { Transition } from 'vue';
 
 const BACKGROUNDS = [
-  styles.background2, styles.background3, styles.background4, styles.background5, styles.background6,
-  styles.bgneo, styles.bgneoalt
+  styles.background2, styles.background3,
 ];
 
 export default defineComponent({
